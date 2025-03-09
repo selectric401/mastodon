30e71196a072c0b1ca2b5465d60f68b8517caa80
Liz Mulholland <liz@selectric.space>
2025-03-09 00:13:00 +0000

    content security policy hacks

--- app/lib/content_security_policy.rb
+++ app/lib/content_security_policy.rb
@@ -36,7 +36,7 @@ class ContentSecurityPolicy
   end
 
   def cdn_host_value
-    s3_alias_host || s3_cloudfront_host || azure_alias_host || s3_hostname_host || swift_object_url
+    s3_alias_host || s3_cloudfront_host || azure_alias_host || s3_hostname_host || swift_object_url || extra_data_hosts
   end
 
   def paperclip_root_url
@@ -72,6 +72,10 @@ class ContentSecurityPolicy
     host_to_url ENV.fetch('S3_HOSTNAME', nil)
   end
 
+  def extra_data_hosts
+    host_to_url ENV.fetch('EXTRA_DATA_HOSTS', nil)
+  end
+  
   def swift_object_url
     url = ENV.fetch('SWIFT_OBJECT_URL', nil)
     return if url.blank? || !url.start_with?('https://')
